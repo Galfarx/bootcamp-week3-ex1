45dcf1ed8543de2c576ec00b064981c92155d0d6
Dominik Kubis <kubis.dominik1@gmail.com>
2017-01-08 19:39:57 +0100

    Initial commit

---
+++ src/scripts/scripts.js
@@ -0,0 +1,68 @@
+(function () {
+    // function Person(firstName, lastName) {
+    //
+    //     if( !(this instanceof Person) ) {
+    //         return new Person(firstName, lastName);
+    //     }
+    //
+    //     this.firstName = firstName;
+    //     this.lastName = lastName;
+    //
+    // }
+    //
+    // Person.prototype.sayHello = function() {
+    //
+    //     console.log("Cześć, nazywam się " + this.firstName + " " + this.lastName + "!");
+    //
+    // };
+    //
+    // function Programmer(firstName, lastName, lang) {
+    //
+    //     lang = lang || "JavaScript";
+    //
+    //     Person.call(this, firstName, lastName);
+    //
+    //     this.lang = lang;
+    //
+    // };
+    //
+    // Programmer.prototype = Object.create(Person.prototype);
+    // Programmer.prototype.constructor = Programmer;
+    //
+    // Programmer.prototype.work = function() {
+    //
+    //     this.sayHello();
+    //     console.log("Piszę właśnie kod w moim ulubionym języku, którym jest " + this.lang + ".");
+    //
+    // };
+    //
+    // var jsDev = new Programmer("Jan", "Kowalski", "Python");
+    //
+    // jsDev.work();
+
+    class Person {
+        constructor(firstName, lastName) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        sayHello() {
+            console.log("Cześć, nazywam się " + this.firstName + " " + this.lastName + "!");
+        }
+    }
+
+    class Programmer extends Person{
+        constructor(firstName, lastName, lang = "JavaScript") {
+            super(firstName, lastName);
+            this.lang = lang;
+        }
+
+        work() {
+            super.sayHello();
+            console.log("Piszę właśnie kod w moim ulubionym języku, którym jest " + this.lang + ".");
+        }
+    }
+    var jsDev = new Programmer("Jan", "Kowalski", "Python");
+
+    jsDev.work();
+})();
